f0713572b266ffbd661d1ce6fc6d4d82b2705bcc
Casey Oates <CAOates@uncc.edu>
2022-04-14 12:28:36 -0400

    Testing AWS deployment - elastic beanstalk and code pipeline
    I hope it works this time.

--- application.py
+++ application.py
@@ -89,7 +89,10 @@ def logout():
         session.clear()
     return redirect(url_for('home'))
 
+
+#Needed to run locally
 #app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=False)
-#new stuff
 
-
+#Needed to run on Elastic Beanstalk
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
